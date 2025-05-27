1e7786f11bd4ec223a8ab3823febad58d0e5373b
bandeau <augusta.bande@ext.ec.europa.eu>
2025-05-27 16:03:50 +0200

    fix: added favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -13,10 +13,10 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
          {/* seo begin */}
-        <title>Jable Studio | Desarrollo web en Fuerteventura y consultoría digital informática</title>
+        <title>Studio Jable | Desarrollo web y consultoría digital informática en Fuerteventura</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Agencia digital en Fuerteventura especializada en desarrollo web, soluciones digitales personalizadas y consultoría informática para empresas y autónomos. Estamos en Lajares." />
-        <meta name="keywords" content="desarrollo web Fuerteventura, desarrollo web Lajares, consultoría informática Canarias, soluciones digitales Fuerteventura, agencia web Lajares, diseñador web fuerteventura, páginas web canarias, experto wordpress fuerteventura, servicios digitales pymes" />
+        <meta name="keywords" content="desarrollo web Fuerteventura, desarrollo web Lajares, desarrollo web Canarias, consultoría informática Canarias, soluciones digitales Fuerteventura, agencia web Lajares, diseñador web fuerteventura, páginas web canarias, experto wordpress fuerteventura, servicios digitales pymes" />
         {/* seo end */}   
           	
 	      <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/e1734ea95b2432fcaa34569d/script.js"></script>
