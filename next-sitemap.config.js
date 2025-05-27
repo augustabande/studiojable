01e4609774ed3567566e37d771280b797c69edf2
bandeau <augusta.bande@ext.ec.europa.eu>
2025-05-27 14:14:52 +0200

    fix: added img and icons

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.studiojable.com', // sostituisci con il tuo dominio
+  generateRobotsTxt: true,
+  sitemapSize: 7000, // default va bene
+  changefreq: 'weekly',
+  priority: 0.7,
+}
