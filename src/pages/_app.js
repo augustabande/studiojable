5ffbfd6ef531781a0c02f6846e8975a2dd869445
bandeau <augusta.bande@ext.ec.europa.eu>
2025-06-01 17:03:10 +0200

    fix: google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -24,18 +24,24 @@ function MyApp({ Component, pageProps }) {
         {/* seo end */}   
           	
 	      <script id="cookieyes" strategy="lazyOnload" type="text/javascript" src="https://cdn-cookieyes.com/client_data/e1734ea95b2432fcaa34569d/script.js"></script>
-        <Script
-          id="gtm-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PP3JZXXM');`
-          }}
-        />
       </Head>
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-42BJ9ER9DD');
+          `
+        }}
+      />   
       <Component {...pageProps} />
     </>
   );
