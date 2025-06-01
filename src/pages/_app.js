4895df4c91764d1c26e4a4392052b16bf0545ad0
bandeau <augusta.bande@ext.ec.europa.eu>
2025-06-01 11:45:37 +0200

    fix: added GTAG

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 
 import '../styles/scss/style.scss';
 import "../styles/globals.css";
+import Script from "next/script";
 
 import { register } from "swiper/element/bundle";
 // register Swiper custom elements
@@ -23,7 +24,17 @@ function MyApp({ Component, pageProps }) {
         {/* seo end */}   
           	
 	      <script id="cookieyes" strategy="lazyOnload" type="text/javascript" src="https://cdn-cookieyes.com/client_data/e1734ea95b2432fcaa34569d/script.js"></script>
-
+        <Script
+          id="gtm-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PP3JZXXM');`
+          }}
+        />
       </Head>
       <Component {...pageProps} />
     </>
